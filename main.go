8b3b8d48ace1e9dc3ede193a18859b22d4352450
Julia Ogris <julia.ogris@gmail.com>
2024-09-19 10:34:52 +1000

    hsl with hue 0-3600

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+    "fmt"
     "image/color"
     "math"
     "time"
@@ -17,7 +18,7 @@ const numPixels = 20
 
 // var colorWriter ColorWriter - defined in main_tiny.go and main_biggy.go
 
-func main() {
+func main2() {
     idx := 0
     inc := 1
     for true {
@@ -33,6 +34,55 @@ func main() {
     }
 }
 
+func main() {
+    h := 0.0
+    s := 1.0
+    l := 0.5
+
+    fmt.Printf("red:\n")
+    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
+    h = h * 10
+    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl2(h, s, l))
+
+    fmt.Printf("\ngreen:\n")
+    h = 120.0
+    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
+    h = h * 10
+    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl2(h, s, l))
+
+    fmt.Printf("\nmagenta:\n")
+    h = 300.0
+    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
+    h = h * 10
+    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl2(h, s, l))
+
+    fmt.Printf("\nlight cyan:\n")
+    h = 180.0
+    l = 0.8
+    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
+    h = h * 10
+    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl2(h, s, l))
+
+    fmt.Printf("\ngrey:\n")
+    h = 300.0
+    s = 0.0
+    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
+    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h*10, s, l, hsl2(h, s, l))
+}
+
+func writeColors(idx int, cw ColorWriter) error {
+    var colors = make([]color.RGBA, numPixels)
+    fidx := float64(idx)
+    for i := range colors {
+        fi := float64(i)
+        distIdx := int(math.Abs(fi - fidx))
+        hue := fi / numPixels * 360
+        lightness := cw.getLightness(distIdx)
+        colors[i] = hsl(hue, 1, lightness)
+    }
+    return cw.WriteColors(colors)
+}
+
 // hsl converts and HSL color to a color.RGBA.
 // h is the hue, an angle in [0,360] s,l in [0,1]
 // see: https://stackoverflow.com/a/64090995/661500
@@ -46,15 +96,15 @@ func hsl(h, s, l float64) color.RGBA {
     return color.RGBA{R: f(0), G: f(8), B: f(4)}
 }
 
-func writeColors(idx int, cw ColorWriter) error {
-    var colors = make([]color.RGBA, numPixels)
-    fidx := float64(idx)
-    for i := range colors {
-        fi := float64(i)
-        distIdx := int(math.Abs(fi - fidx))
-        hue := 0.0 // fi / numPixels * 360
-        lightness := cw.getLightness(distIdx)
-        colors[i] = hsl(hue, 1, lightness)
+// hsl2 converts and HSL color to a color.RGBA.
+// h is the hue, an angle in [0,3600] s,l in [0,1000]
+// see: https://stackoverflow.com/a/64090995/661500
+func hsl2(h, s, l float64) color.RGBA {
+    a := s * min(l, 1-l) / 1
+    f := func(n float64) uint8 {
+        k := math.Mod(n+h/300, 12)
+        v := l - a*max(min(k-3, 9-k, 1), -1)
+        return uint8(v * 255)
     }
-    return cw.WriteColors(colors)
+    return color.RGBA{R: f(0), G: f(8), B: f(4)}
 }
