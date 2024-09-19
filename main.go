41097f71e91f9091cd26662ea919615eb23a6269
Julia Ogris <julia.ogris@gmail.com>
2024-09-19 11:08:05 +1000

    hs int l float

--- main.go
+++ main.go
@@ -35,39 +35,26 @@ func main2() {
 }
 
 func main() {
-    h := 0.0
-    s := 1.0
-    l := 0.5
+    testHSL("red", 0, 1, 0.5)
+    testHSL("green", 120, 1, 0.5)
+    testHSL("magenta", 300, 1, 0.5)
+    testHSL("light cyan", 180, 1, 0.8)
+    testHSL("grey", 300, 0, 0.8)
+}
 
-    fmt.Printf("red:\n")
-    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
-    h = h * 10
-    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl2(h, s, l))
-
-    fmt.Printf("\ngreen:\n")
-    h = 120.0
-    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
-    h = h * 10
-    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl2(h, s, l))
-
-    fmt.Printf("\nmagenta:\n")
-    h = 300.0
-    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
-    h = h * 10
-    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl2(h, s, l))
-
-    fmt.Printf("\nlight cyan:\n")
-    h = 180.0
-    l = 0.8
-    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
-    h = h * 10
-    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl2(h, s, l))
-
-    fmt.Printf("\ngrey:\n")
-    h = 300.0
-    s = 0.0
-    fmt.Printf(" hsl(%7.2f %.2f %.2f) as RGBA %v\n", h, s, l, hsl(h, s, l))
-    fmt.Printf("hsl2(%7.2f %.2f %.2f) as RGBA %v\n", h*10, s, l, hsl2(h, s, l))
+func testHSL(label string, h, s, l float64) {
+    fmt.Printf("\n%s:\n", label)
+    rgb := hsl(h, s, l)
+    fmt.Printf(" hsl(%7.0f %7.2f %7.2f) as RGBA %v\n", h, s, l, rgb)
+    h2 := int(h * 10)
+    s2 := int(s * 1000)
+    l2 := int(l * 1000)
+    rgb2 := hsl2(h2, s2, l2)
+    // s = s * 1000
+    fmt.Printf("hsl2(%7d %7d %7d) as RGBA %v\n", h2, s2, l2, rgb2)
+    if rgb != rgb2 {
+        fmt.Printf("💥 %v != %v\n", rgb, rgb2)
+    }
 }
 
 func writeColors(idx int, cw ColorWriter) error {
@@ -99,12 +86,21 @@ func hsl(h, s, l float64) color.RGBA {
 // hsl2 converts and HSL color to a color.RGBA.
 // h is the hue, an angle in [0,3600] s,l in [0,1000]
 // see: https://stackoverflow.com/a/64090995/661500
-func hsl2(h, s, l float64) color.RGBA {
-    a := s * min(l, 1-l) / 1
-    f := func(n float64) uint8 {
-        k := math.Mod(n+h/300, 12)
+func hsl2(h, s, l int) color.RGBA {
+    l2 := float64(l)
+    a2 := float64(s) * min(l2, 1000-l2) / 1000
+
+    a := s * min(l, 1000-l) / 1000
+    f := func(n int) uint8 {
+        //k := math.Mod(n+h/3, 1200) / 100
+        //k := math.Mod(n+h, 3600) / 300
+        k := (n + h) % 3600 / 300
+        k2 := float64(k)
+        _ = (l2 - a2*max(min(k2-3, 9-k2, 1), -1))
+
         v := l - a*max(min(k-3, 9-k, 1), -1)
-        return uint8(v * 255)
+
+        return uint8(v * 255 / 1000)
     }
-    return color.RGBA{R: f(0), G: f(8), B: f(4)}
+    return color.RGBA{R: f(0), G: f(2400), B: f(1200)}
 }
